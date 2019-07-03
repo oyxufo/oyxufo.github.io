9b799f6f2837dd9d5dab2cb6436aad9c95d50f63
oyxufo <1253928991@qq.com>
2019-07-04 00:00:38 +0800

    Site updated: 2019-07-04 00:00:35

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@ importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.3.0/workbox
 self.addEventListener('install', function (event) {
   event.waitUntil(
     caches.keys().then(function (names) {
-      var validSets = ["is-sw-12c1f8","is-html-12c1f8"];
+      var validSets = ["is-sw-920f98","is-html-920f98"];
       return Promise.all(
         names
           .filter(function (name) { return !~validSets.indexOf(name); })
@@ -16,7 +16,7 @@ self.addEventListener('install', function (event) {
 });
 
 workbox.routing.registerRoute(new RegExp('sw\\.js'), workbox.strategies.networkOnly({
-  cacheName: 'is-sw-12c1f8',
+  cacheName: 'is-sw-920f98',
 }));
 
 workbox.routing.registerRoute(new RegExp('/.*(:?/[^\\.]*/?)$'), function(context) {
