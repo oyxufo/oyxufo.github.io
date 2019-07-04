32939bb318c27ad21adbbe99dc0b16e1575c971a
oyxufo <1253928991@qq.com>
2019-07-04 12:47:23 +0800

    Site updated: 2019-07-04 12:47:21

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@ importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.3.0/workbox
 self.addEventListener('install', function (event) {
   event.waitUntil(
     caches.keys().then(function (names) {
-      var validSets = ["is-sw-920f98","is-html-920f98"];
+      var validSets = ["is-sw-c82360","is-html-c82360"];
       return Promise.all(
         names
           .filter(function (name) { return !~validSets.indexOf(name); })
@@ -16,7 +16,7 @@ self.addEventListener('install', function (event) {
 });
 
 workbox.routing.registerRoute(new RegExp('sw\\.js'), workbox.strategies.networkOnly({
-  cacheName: 'is-sw-920f98',
+  cacheName: 'is-sw-c82360',
 }));
 
 workbox.routing.registerRoute(new RegExp('/.*(:?/[^\\.]*/?)$'), function(context) {
